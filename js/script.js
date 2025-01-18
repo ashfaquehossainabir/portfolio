de99eccefb614d3b7c0403980e1117f2c71ecd2b
Ashfaque Hossain Abir <109417020+ashfaquehossainabir@users.noreply.github.com>
2025-01-18 18:42:39 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -96,4 +96,45 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.project-card, .testimonial-card',{ interval: 200});
+sr.reveal('.project-card, .testimonial-card', { interval: 200});
+
+
+// ==========================================================
+// -- Testimonial Slider
+// ==========================================================
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  centeredSlides: true,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  //   pauseOnMouseEnter: true,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      780: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
