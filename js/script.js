8b0d858ea6614217f97951deb14007f26dad9317
Ashfaque Hossain Abir <109417020+ashfaquehossainabir@users.noreply.github.com>
2025-01-19 23:56:06 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 // -- Preloader Functionalities
 // ======================================================
 
-// $(window).on("load", function () {
-//   "use strict";
+$(window).on("load", function () {
+  "use strict";
 
-//   // Preloader Fadeout
-//   $("#preloader").delay(350).fadeOut("slow");
-// });
+  $("#preloader").delay(350).fadeOut("slow");
+  // Because only Chrome supports offset-path, feGaussianBlur for now
+  var isChrome =
+    /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+  if (!isChrome) {
+    document.getElementsByClassName("infinityChrome")[0].style.display = "none";
+    document.getElementsByClassName("infinity")[0].style.display = "block";
+}});
 
 
 // =======================================================
