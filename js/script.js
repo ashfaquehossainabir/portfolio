852b015cd9cbf7cd18d37ce287974fd942bea638
Ashfaque Hossain Abir <109417020+ashfaquehossainabir@users.noreply.github.com>
2025-02-16 22:04:08 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -187,3 +187,106 @@ const yearText = document.getElementById("year");
 
 let year = date.getFullYear();
 yearText.innerHTML = year;
+
+
+// ==========================================================
+// -- Set url attribute in image
+// ==========================================================
+
+const projectImageOne = document.getElementById("project-img-one")
+const projectLinkOne = document.getElementById("project-link-one")
+const projectTextOne = document.getElementById("project-text-one")
+
+const projectImageTwo = document.getElementById("project-img-two")
+const projectLinkTwo = document.getElementById("project-link-two")
+const projectTextTwo = document.getElementById("project-text-two")
+
+const projectImageThree = document.getElementById("project-img-three")
+const projectLinkThree = document.getElementById("project-link-three")
+const projectTextThree = document.getElementById("project-text-three")
+
+const projectImageFour = document.getElementById("project-img-four")
+const projectLinkFour = document.getElementById("project-link-four")
+const projectTextFour = document.getElementById("project-text-four")
+
+const projectImageFive = document.getElementById("project-img-five")
+const projectLinkFive = document.getElementById("project-link-five")
+const projectTextFive = document.getElementById("project-text-five")
+
+const projectImageSix = document.getElementById("project-img-six")
+const projectLinkSix = document.getElementById("project-link-six")
+const projectTextSix = document.getElementById("project-text-six")
+
+
+const labelContentOne = document.getElementById("label-content-one")
+const thumbnailHeadlineOne = document.getElementById("thumbnail-headline-one")
+
+const labelContentTwo = document.getElementById("label-content-two")
+const thumbnailHeadlineTwo = document.getElementById("thumbnail-headline-two")
+
+const labelContentThree = document.getElementById("label-content-three")
+const thumbnailHeadlineThree = document.getElementById("thumbnail-headline-three")
+
+const labelContentFour = document.getElementById("label-content-four")
+const thumbnailHeadlineFour = document.getElementById("thumbnail-headline-four")
+
+const labelContentFive = document.getElementById("label-content-five")
+const thumbnailHeadlineFive = document.getElementById("thumbnail-headline-five")
+
+const labelContentSix = document.getElementById("label-content-six")
+const thumbnailHeadlineSix = document.getElementById("thumbnail-headline-six")
+
+
+
+// Project 1
+projectImageOne.setAttribute('src', './images/project-images/thumbnail.png')
+projectLinkOne.setAttribute('href', 'https://ashfaquertc.github.io/coming-soon')
+projectTextOne.innerHTML = "Coming Soon"
+
+labelContentOne.innerHTML = "Website"
+thumbnailHeadlineOne.innerHTML = "Project Coming Soon"
+
+
+// Project 2
+projectImageTwo.setAttribute('src', './images/project-images/thumbnail.png')
+projectLinkTwo.setAttribute('href', 'https://ashfaquertc.github.io/coming-soon')
+projectTextTwo.innerHTML = "Coming Soon"
+
+labelContentTwo.innerHTML = "Website"
+thumbnailHeadlineTwo.innerHTML = "Project Coming Soon"
+
+
+// Project 3
+projectImageThree.setAttribute('src', './images/project-images/thumbnail.png')
+projectLinkThree.setAttribute('href', 'https://ashfaquertc.github.io/coming-soon')
+projectTextThree.innerHTML = "Coming Soon"
+
+labelContentThree.innerHTML = "Website"
+thumbnailHeadlineThree.innerHTML = "Project Coming Soon"
+
+
+// Project 4
+projectImageFour.setAttribute('src', './images/project-images/thumbnail.png')
+projectLinkFour.setAttribute('href', 'https://ashfaquertc.github.io/coming-soon')
+projectTextFour.innerHTML = "Coming Soon"
+
+labelContentFour.innerHTML = "Website"
+thumbnailHeadlineFour.innerHTML = "Project Coming Soon"
+
+
+// Project 5
+projectImageFive.setAttribute('src', './images/project-images/thumbnail.png')
+projectLinkFive.setAttribute('href', 'https://ashfaquertc.github.io/coming-soon')
+projectTextFive.innerHTML = "Coming Soon"
+
+labelContentFive.innerHTML = "Website"
+thumbnailHeadlineFive.innerHTML = "Project Coming Soon"
+
+
+// Project 6
+projectImageSix.setAttribute('src', './images/project-images/thumbnail.png')
+projectLinkSix.setAttribute('href', 'https://ashfaquertc.github.io/coming-soon')
+projectTextSix.innerHTML = "Coming Soon"
+
+labelContentSix.innerHTML = "Website"
+thumbnailHeadlineSix.innerHTML = "Project Coming Soon"
